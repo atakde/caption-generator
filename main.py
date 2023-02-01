bc61c548b0c823524e1c954bb5fe09d1af47a6aa
Atakan Demircioğlu <31544037+atakde@users.noreply.github.com>
2023-02-01 16:05:58 +0300

    main :: better comments for docs

--- main.py
+++ main.py
@@ -9,18 +9,12 @@ os.environ.get("REPLICATE_API_TOKEN")
 class Item(BaseModel):
     image: str
 
-@app.post("/")
-def read_root(item: Item):
+@app.post("/", summary="Generate a caption for an image", tags=["Generate Caption"])
+def generate(item: Item):
     model = replicate.models.get("salesforce/blip")
     version = model.versions.get("2e1dddc8621f72155f24cf2e0adbde548458d3cab9f00c0139eea840d0ac4746")
-
-    if item.image is None:
-        return {"error": "image is required"}
-
-    inputs = {
+    output = version.predict({
         'image': item.image,
         'task': "image_captioning",
-    }
-
-    output = version.predict(**inputs)
+    })
     return output
