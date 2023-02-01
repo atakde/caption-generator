6775b263111f89f67bc3cb36a24b1b45c55bb35a
Atakan Demircioğlu <31544037+atakde@users.noreply.github.com>
2023-02-01 15:59:54 +0300

    post endpoint

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 import os
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
+from pydantic import BaseModel
 import replicate
 app = FastAPI()
 
 os.environ.get("REPLICATE_API_TOKEN")
 
-@app.get("/")
-def read_root(request: Request):
+class Item(BaseModel):
+    image: str
+
+@app.post("/")
+def read_root(item: Item):
     model = replicate.models.get("salesforce/blip")
     version = model.versions.get("2e1dddc8621f72155f24cf2e0adbde548458d3cab9f00c0139eea840d0ac4746")
-    type = request.query_params.get("type") or "url" # todo: add support for base64
-    image =  request.query_params.get("image");
 
-    if image is None:
+    if item.image is None:
         return {"error": "image is required"}
 
     inputs = {
-        # Input image
-        'image': image,
-        # Choose a task.
+        'image': item.image,
         'task': "image_captioning",
     }
 
